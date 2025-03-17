8538120ba9dba68700d7d5714d44df509fa8e0ef
Сергей <109113135+Sergey-Ivch@users.noreply.github.com>
2025-03-17 19:25:14 +0300

    Update views.py

--- recipe/users/views.py
+++ recipe/users/views.py
@@ -5,6 +5,6 @@ from .forms import CreationForm
 class SignUp(CreateView):
     form_class = CreationForm
     # После успешной регистрации перенаправляем пользователя на главную.
-    success_url = reverse_lazy('ricipt:index')
+    success_url = reverse_lazy('recipt:index')
     template_name = 'users/signup.html'
 
